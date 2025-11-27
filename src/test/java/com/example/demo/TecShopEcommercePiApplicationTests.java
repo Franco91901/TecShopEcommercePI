622d57911308728c6543325b2013fe5db0149f45
Franco91901 <francoarp15@gmail.com>
2025-11-27 00:22:35 -0500

    Primer commit: proyecto Spring Boot

---
+++ src/test/java/com/example/demo/TecShopEcommercePiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecShopEcommercePiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
